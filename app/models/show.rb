ce64d7d6b3a1677948d4e6799b98a5db74329325
minirachel <rachel.a.levinson@gmail.com>
2021-05-20 11:44:59 -0400

    commit tree 3b99a46a0efb7941f5f0f4f29bb2645bca739fc1

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,14 @@
+require 'pry'
+
 class Show < ActiveRecord::Base
-    belongs_to :networks
+    belongs_to :network
+    has_many :characters
     has_many :actors, through: :characters
+
+    def actors_list
+        self.characters.map do |c|
+            Actor.find(c.actor_id).full_name
+        end
+    end
+
 end
