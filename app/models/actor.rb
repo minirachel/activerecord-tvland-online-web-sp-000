18a3af02eafcca4a8e01fd0144823c4564b6e972
minirachel <rachel.a.levinson@gmail.com>
2021-05-20 10:38:56 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        [self.first_name, self.last_name].join(' ')
+    end
+
+    # def list_roles
+    #     binding.pry
+    #     self.characters.map do |c|
+    #         c.name - c.show_id
+    #     end
+    # end
 end
